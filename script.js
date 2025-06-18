54f9f21fe001e37eb9b839563b73f3cd195f3be5
siddardha95 <venkatsiddardha@gmail.com>
2025-06-18 10:37:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-// document.querySelectorAll('.nav-links a[href^="#"]').forEach((link) => {
-//   link.addEventListener('click',(e) => {
-//     e.preventDefault();
-
-//     document.querySelector(link.getAttribute('href')).scrollIntoView({behavior:'smooth'})
-//   });
-// });
-
-// // Change navbar background on scroll
-// window.addEventListener("scroll", function () {
-//   const navbar = document.querySelector(".navbar");
-
-//   if (window.scrollY > 20) {
-//     navbar.style.backgroundColor = "#3d2b1f";
-//   } else {
-//     navbar.style.backgroundColor = "#5b3b28";
-//   }
-// });
-// Smooth scroll on nav link click
 document.querySelectorAll('.nav-links a[href^="#"]').forEach((link) => {
   link.addEventListener('click', (e) => {
     e.preventDefault();
